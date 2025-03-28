4bcfbedc9352af2ba01ae77bbdf9a0b76248af47
Dmitry <pikachyshkaa@gmail.com>
2025-03-28 17:54:19 +0500

    fix bag

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -44,18 +44,16 @@ def add_url():
 
     if not url or len(url) > 255 or not validators.url(url):
         flash('Некорректный URL', 'danger')
-        return redirect(url_for('index'))
+        return render_template('index.html', error="Некорректный URL"), 422
 
     try:
         with get_db() as conn, conn.cursor() as cur:
-            cur.execute('SELECT id FROM urls WHERE LOWER(TRIM(name))'
-                        ' = LOWER(TRIM(%s))', (url,))
+            cur.execute('SELECT id FROM urls WHERE LOWER(TRIM(name)) = LOWER(TRIM(%s))', (url,))
             if existing := cur.fetchone():
                 flash('Страница уже существует', 'info')
                 return redirect(url_for('url_detail', id=existing[0]))
 
-            cur.execute('INSERT INTO urls (name) '
-                        'VALUES (%s) RETURNING id', (url,))
+            cur.execute('INSERT INTO urls (name) VALUES (%s) RETURNING id', (url,))
             new_id = cur.fetchone()[0]
             flash('Страница успешно добавлена', 'success')
             return redirect(url_for('url_detail', id=new_id))
