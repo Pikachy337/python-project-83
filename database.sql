3fbf3b0db75aef00f384e5b8974126297cf7f487
Dmitry <pikachyshkaa@gmail.com>
2025-03-28 17:45:38 +0500

    add urls

--- database.sql
+++ database.sql
@@ -1,9 +1,15 @@
+CREATE TABLE IF NOT EXISTS urls (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
 CREATE TABLE IF NOT EXISTS url_checks (
-    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    url_id BIGINT REFERENCES urls(id),
-    status_code INT,
-    h1 VARCHAR(255),
-    title VARCHAR(255),
+    id SERIAL PRIMARY KEY,
+    url_id INTEGER REFERENCES urls(id) ON DELETE CASCADE,
+    status_code INTEGER,
+    h1 TEXT,
+    title TEXT,
     description TEXT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
