03b3ae8622470b1a92be48d5816bcd0bf830d51f
Dmitry <pikachyshkaa@gmail.com>
2025-04-04 21:51:20 +0500

    change start

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
 curl -LsSf https://astral.sh/uv/install.sh | sh
-source $HOME/.cargo/env
-uv pip install .
-psql -a -d $DATABASE_URL -f database.sql
+source $HOME/.local/bin/env
+make install
