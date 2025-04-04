9145fc0b57f0bdb96c62883ef4bb50eb44d779f5
Dmitry <pikachyshkaa@gmail.com>
2025-04-04 16:40:00 +0500

    another try search by domain

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -52,15 +52,14 @@ def add_url():
 
     try:
         with get_db() as conn, conn.cursor() as cur:
-            cur.execute('SELECT id FROM urls'
-                        ' WHERE LOWER(TRIM(name)) = LOWER(TRIM(%s))', (domain,))
+            cur.execute('SELECT id FROM urls WHERE LOWER(TRIM(name)) = LOWER(TRIM(%s))', (domain,))
             existing = cur.fetchone()
+
             if existing:
                 flash('Страница уже существует', 'info')
                 return redirect(url_for('url_detail', id=existing[0]))
 
-            cur.execute('INSERT INTO urls (name)'
-                        ' VALUES (%s) RETURNING id', (domain,))
+            cur.execute('INSERT INTO urls (name) VALUES (%s) RETURNING id', (domain,))
             new_id = cur.fetchone()[0]
             flash('Страница успешно добавлена', 'success')
             return redirect(url_for('url_detail', id=new_id))
