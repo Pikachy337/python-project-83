2b754a603dea656a2b33942b883200b6f0927eff
Dmitry <pikachyshkaa@gmail.com>
2025-04-05 19:01:25 +0500

    change build

--- build.sh
+++ build.sh
@@ -2,5 +2,4 @@
 
 curl -LsSf https://astral.sh/uv/install.sh | sh
 source $HOME/.local/bin/env
-make install
-psql -d $DATABASE_URL -f database.sql
+make install && psql -a -d $DATABASE_URL -f database.sql
