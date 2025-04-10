9a4cd88ef54775db4fd99bf6ee7ea0495db2e7ee
Dmitry <pikachyshkaa@gmail.com>
2025-04-10 21:45:24 +0500

    fix lint

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -3,7 +3,8 @@ import pytest
 
 
 def test_validate_and_normalize_url():
-    assert validate_and_normalize_url("https://example.com") == "https://example.com"
+    assert (validate_and_normalize_url("https://example.com")
+            == "https://example.com")
     assert validate_and_normalize_url("http://test.ru/path") == "http://test.ru"
 
     with pytest.raises(ValueError):
